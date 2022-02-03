67d08f4a0e1f0616ff811cd62ce5664539f81994
Vera Elena Link <fdai6240@fhfddvz1.rz.hs-fulda.de>
2022-02-03 23:06:06 +0100

    add option to add several keys at once

--- Main.java
+++ Main.java
@@ -1,23 +1,51 @@
 //package algoDat_avltree;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
        AVLTree avlTree = new AVLTree();
        
+       System.out.println("\n***** Options *****\n1: Insert single node \n2: Insert several nodes, seperated with ' ' or ',' \n3: Delete [WIP] \n4: Search [WIP] \n5: Print \n6: Quit\n*******************");
        int choice;
        do {
-            System.out.println("\nChoose Operation! (1: Insert, 2: Delete, 3: Search, 4: Print, 5: Quit)");
+            System.out.print("\noption: ");
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Enter a value to insert: ");
+                    System.out.print("key: ");
                     avlTree.insert(scanner.nextInt());
                     break;
                     
-                case 2:
+                case 2: 
+                    System.out.print("keys: ");
+                    scanner.nextLine();
+                    String input = scanner.nextLine() + " ";
+                    ArrayList<Integer> nodes = new ArrayList<Integer>();
+
+                    // cast input String to Integer-ArrayList
+                    String number = "";
+                    for(int i = 0; i < input.length(); i++) {
+                        char digit = input.charAt(i);
+                        if (digit != ' ' && digit != ',') {
+                            number += digit;
+                        } else if (!number.isEmpty()) {
+                            nodes.add(Integer.parseInt(number));
+                            number = "";
+                        }
+
+                    }
+                    // add nodes to tree
+                    for(Integer node : nodes) {
+                        avlTree.insert(node);
+                    }
+
+                    break;
+                    
+                case 3:
                     System.out.println("Operation not supported yet...");
                     /*
                     System.out.println("\nEnter a value to delete: ");
@@ -25,7 +53,7 @@ public class Main {
                     */
                     break;
                     
-                case 3:
+                case 4:
                     System.out.println("Operation not supported yet...");
                     /*
                     System.out.println("\nEnter a value to search: ");
@@ -34,13 +62,13 @@ public class Main {
                     */
                     break;
                     
-                case 4:
+                case 5:
                     System.out.print("\nMy AVL-Tree: ");
                     avlTree.printMytree();
                     System.out.println();
                     break;
                 
-                case 5:
+                case 6:
                     System.out.println("Good bye! :-)"); 
                     break;
                     
@@ -48,7 +76,7 @@ public class Main {
                     System.out.println("Please choose correct option! \n ");                      
             }
                       
-        } while (choice != 5);
+        } while (choice != 6);
         scanner.close();
     }
 }
